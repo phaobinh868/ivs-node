f7c5b4bea10b33075cafab53d32092b54b2256dc
DanhVT <tintin.thanhdanh@gmail.com>
2022-08-03 14:54:15 +0700

    cross origin

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ const main = async () => {
   
   app.use(AWSXRay.express.openSegment(process.env.SERVICE_NAME || 'backend'));
   
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN);
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-
   app = routers(app);
   app.get('*', (req, res) => {
     const url = req.url.split("?");
